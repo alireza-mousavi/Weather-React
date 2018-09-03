d7e9510dbfa37f1b1243d613614fb80b89e5018f
“Alireza <k1504190@kcl.ac.uk>
2018-09-03 19:44:48 +0100

    Simple refactoring of some components

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,13 +1,10 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-  render : function () {
-    var {temp, location} = this.props;
-    
-    return (
-      <h2>Its {temp} degrees in {location}</h2>
-    )
-  },
-});
+//Destructuring inside arguments!
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <h2>Its {temp} degrees in {location}</h2>
+  )
+};
 
 module.exports = WeatherMessage;
