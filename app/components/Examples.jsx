aba506d55149f57ea8037f5ddd0ba1c3ad11532f
“Alireza <k1504190@kcl.ac.uk>
2018-09-04 14:09:40 +0100

    centered componenets in the main module

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
-  )
+    <div>
+      <h1 className='text-cdnter'>Examples</h1>
+      <p>Here are a few example locations to try out: </p>
+      <ol>
+        <li>
+          <Link to ='/?location=London'>London, UK</Link>
+        </li>
+        <li>
+          <Link to='/?location=Tehran'>Tehran, Iran</Link>
+        </li>
+      </ol>
+    </div>
+  );
 };
 
 module.exports = Examples;
