b298c15cadebac519785e6719bdbf76b5d06be68
“Alireza <k1504190@kcl.ac.uk>
2018-09-04 14:25:05 +0100

    style to about page and links to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,25 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <p3>Welcome to the about page.</p3>
+      <h1 className='text-center'>About</h1>
+
+      <p>
+        This is a weather application build on React and hoseted on Heraku.
+      </p>
+      <p>
+        Tools used:
+        <ul>
+          <li>
+            <a href='https://facebook.github.io/react'>React</a> - JavaScript framework
+          </li>
+          <li>
+            <a href='https://openweathermap.org/'>Open Weather Map</a> - Weather API used in order to fetch the weather
+          </li>
+          <li>
+            <a href='https://www.heroku.com/'>Heroku</a> - Cloud platform on which this app is hoseted on
+          </li>
+        </ul>
+      </p>
     </div>
   )
 };
