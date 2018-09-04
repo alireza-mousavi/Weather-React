9903bc00fa1dbb0beda4f644058aebad818d4a82
“Alireza <k1504190@kcl.ac.uk>
2018-09-04 11:56:08 +0100

    update server.js '

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 
-//create our app
+// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
+app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
@@ -14,6 +14,6 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
-app.listen(PORT, function(){
-    console.log('Express sever is up on port ' + PORT);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
