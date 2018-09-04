a599e8a86c213b405cfc2ac3f8e96b8b8fd4161b
“Alireza <k1504190@kcl.ac.uk>
2018-09-04 14:31:00 +0100

    add style to weather page '

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 //Destructuring inside arguments!
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h2>Its {temp} degrees in {location}</h2>
+    <h2 className='text-center'>Its {temp} degrees in {location}</h2>
   )
 };
 
